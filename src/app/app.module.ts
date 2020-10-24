2c04a4f4276cdc56d251b34e43f58f0a37111fc5
PoulaHelmy <bolahelmy2016@gmail.com>
2020-10-24 23:58:44 +0200

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+// Imports for loading & configuring the in-memory web api
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ProductData } from './products/product-data';
+
 import { AppComponent } from './app.component';
+import { ShellComponent } from './home/shell.component';
+import { MenuComponent } from './home/menu.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './home/page-not-found.component';
+/* Feature Modules */
+import { UserModule } from './user/user.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(ProductData),
+    UserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+  ],
+  declarations: [
+    AppComponent,
+    ShellComponent,
+    MenuComponent,
+    WelcomeComponent,
+    PageNotFoundComponent,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
